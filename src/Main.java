3c999944d23b45ccbac2e560783625537abadf5e
Boris <carrera1961@mail.ru>
2022-09-09 03:02:26 +0700

    Коммит

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+//Задание 1
+    public static void main(String[] args) {
+    int age = 16;
+        if (age >= 18){
+            System.out.println("Поздравляю!");
+
+        }
+    }
+}
